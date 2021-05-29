d603cdd00ebe2f67d74f8553fdfb728fdda7520c
Oofermanno <79939114+Oofermanno@users.noreply.github.com>
2021-05-28 23:41:10 -0400

    Update Module.lua

--- Module.lua
+++ Module.lua
@@ -5,11 +5,11 @@ animator.__index = animator
 function animator.loadPresets()
 	local presets = {}
 	setmetatable(presets, animator)
-presets.Robust = 0.05
-presets.Smooth = 0.01
-presets.Fast = 0.1
+	presets.Robust = 0.05
+	presets.Smooth = 0.01
+	presets.Fast = 0.1
 
-return presets
+	return presets
 end
 function animator:getPreset(preset)
 	print(self[preset])
@@ -28,7 +28,7 @@ function animator:setPart(part)
 end
 function animator:getPart()
 	return self.Part
-	
+
 end
 function animator:setValue(value)
 	self.ChangeTo = value
@@ -48,45 +48,45 @@ end
 function animator:isTransformingSize(value)
 	return self.changeSize
 end
-function animator.datamodel(newvalues)
+function animator.datamodel(part,newval,speed,transform,isweld)
 	local properties = {}
-	if newvalues ~= nil then
-	for i,v in pairs(newvalues) do
-		
-properties[i] = v
-		end
+	if isweld ~= nil then
+		properties.Part = part
+		properties.ChangeTo = newval
+		properties.Speed = speed
+		properties.changeSize = transform
+		properties.isWeld = isweld
 	end
 	setmetatable(properties,animator)
-return properties
+	return properties
 end
 
 
 
 function animator.new(prop)
-local part = prop.Part
-local newValue = prop.ChangeTo
-local speed = prop.Speed
-local isSize = prop.changeSize
-local isWeld = prop.isWeld
-for i=0,1,speed do
-if isWeld == true then
-part.C0 = part.C0:lerp(newValue,i)
-else
-if typeof(newValue) == "Vector3" then
-if isSize == true then
-	part.Size = part.Size:lerp(newValue,i)
-else
-	
-	part.Position = part.Position:lerp(newValue,i)
-	
+	local part = prop.Part
+	local newValue = prop.ChangeTo
+	local speed = prop.Speed
+	local isSize = prop.changeSize
+	local isWeld = prop.isWeld
+	for i=0,1,speed do
+		if isWeld == true then
+			part.C0 = part.C0:lerp(newValue,i)
+		else
+			if typeof(newValue) == "Vector3" then
+				if isSize == true then
+					part.Size = part.Size:lerp(newValue,i)
+				else
+
+					part.Position = part.Position:lerp(newValue,i)
+
+				end
+			else
+				part[typeof(newValue)] = part[typeof(newValue)]:lerp(newValue,i)
+			end
+			RS.Stepped:Wait()
+		end
 	end
-else
-part[typeof(newValue)] = part[typeof(newValue)]:lerp(newValue,i)
-end
-RS.Stepped:Wait()
-end
-end
 end
 
 return animator
-
